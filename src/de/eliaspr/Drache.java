25176cf7c275205efbaaed40eee718f934f670ee
Elias Hoerner <elias@eliashoerner.de>
2021-01-14 19:48:06 +0100

    added remaining time & countdown commands

--- src/de/eliaspr/Drache.java
+++ src/de/eliaspr/Drache.java
@@ -2,7 +2,8 @@ package de.eliaspr;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.utils.Compression;
@@ -15,14 +16,12 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.nio.Buffer;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.Random;
+import java.util.*;
 
 public class Drache extends ListenerAdapter {
+
+    private static final ArrayList<String> activeCountdowns = new ArrayList<>();
 
     private static ArrayList<String> messages = new ArrayList<>();
     private static ArrayList<File> photos = new ArrayList<>();
@@ -60,7 +59,81 @@ public class Drache extends ListenerAdapter {
         try {
             if (DiscordBots.checkChannel(event.getChannel()) && !event.getAuthor().isBot()) {
                 String msg = event.getMessage().getContentRaw().toLowerCase().trim();
-                if (msg.contains("etzala") || msg.contains("meddl")) {
+                boolean etzala;
+                if ((etzala = msg.contains("etzala")) || msg.contains("meddl")) {
+                    if (etzala && event.getGuild().getIdLong() == 657602012179070988L) {
+                        if (msg.contains("wi") && msg.contains("lang") && msg.contains("noch")) {
+                            Calendar c = Calendar.getInstance();
+                            int dayAgeMinutes = c.get(Calendar.HOUR_OF_DAY) * 60 + c.get(Calendar.MINUTE);
+                            Lecture lecture = Lecture.getCurrentLecture(event.getGuild());
+                            if (lecture != null) {
+                                int remainingSecnds = (lecture.endTime - dayAgeMinutes) * 60 - c.get(Calendar.SECOND);
+                                log(event, "Sending remaining time of " + lecture.name);
+                                int remainingMinutes = remainingSecnds / 60;
+                                remainingSecnds %= 60;
+                                boolean minigun = remainingMinutes > 30;
+                                int remainingHours = remainingMinutes / 60;
+                                remainingMinutes %= 60;
+                                event.getChannel().sendMessage(
+                                        String.format("%s dauert noch %02d:%02d:%02d %s",
+                                                lecture.name, remainingHours, remainingMinutes, remainingSecnds,
+                                                minigun ? ":pepeMinigun:" : ""
+                                        )).queue();
+                            } else {
+                                event.getChannel().sendMessage("Gerade läuft keine Vorlesung").queue();
+                            }
+                            return;
+                        } else if (msg.contains("mach") && msg.contains("countdown")) {
+                            final Lecture currentLecture = Lecture.getCurrentLecture(event.getGuild());
+                            if (currentLecture != null) {
+                                boolean alreadyStarted;
+                                synchronized (activeCountdowns) {
+                                    alreadyStarted = activeCountdowns.contains(currentLecture.name);
+                                }
+                                if (!alreadyStarted) {
+                                    Thread th = new Thread(() -> {
+                                        synchronized (activeCountdowns) {
+                                            activeCountdowns.add(currentLecture.name);
+                                        }
+                                        event.getChannel().sendMessage("Countdown aktiv ab 15 Minuten :thumbsup:").queue();
+                                        int lastDisplayedRemaining = -1;
+                                        while (true) {
+                                            Calendar c = Calendar.getInstance();
+                                            int dayAgeMinutes = c.get(Calendar.HOUR_OF_DAY) * 60 + c.get(Calendar.MINUTE);
+                                            int remainingMinutes = currentLecture.endTime - dayAgeMinutes;
+                                            if (remainingMinutes != lastDisplayedRemaining) {
+                                                lastDisplayedRemaining = remainingMinutes;
+                                                if(remainingMinutes == 0) {
+                                                    event.getChannel().sendMessage(
+                                                            currentLecture.name + " ist vorbei! :beer: "
+                                                    ).queue();
+                                                    break;
+                                                } else if (remainingMinutes < 15) {
+                                                    event.getChannel().sendMessage(
+                                                            currentLecture.name + " ist in " + remainingMinutes + " " +
+                                                                    (remainingMinutes == 1 ? "Minute" : "Minuten") + " zu Ende!"
+                                                    ).queue();
+                                                }
+                                            }
+                                            try {
+                                                Thread.sleep(1000);
+                                            } catch (InterruptedException e) {
+                                                break;
+                                            }
+                                        }
+                                        synchronized (activeCountdowns) {
+                                            activeCountdowns.remove(currentLecture.name);
+                                        }
+                                    });
+                                    th.setDaemon(true);
+                                    th.start();
+                                }
+                            } else {
+                                event.getChannel().sendMessage("Gerade läuft keine Vorlesung").queue();
+                            }
+                            return;
+                        }
+                    }
                     String answer = messages.get(random.nextInt(messages.size()));
                     log(event, "Sending \"" + answer + "\"");
                     event.getChannel().sendMessage(answer).queue();
@@ -90,13 +163,20 @@ public class Drache extends ListenerAdapter {
 //                        }
 //                    }).start();
 //                }
-                if(msg.contains("exmatrikulation")) {
+                if (msg.contains("exmatrikulation")) {
                     event.getChannel().sendMessage("https://www.mosbach.dhbw.de/service-einrichtungen/pruefungsamt/exmatrikulation/").queue();
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private int parseTimeStamp(String timeStamp) {
+        String[] spl = timeStamp.split(":");
+        int hours = Integer.parseInt(spl[0]);
+        int minutes = Integer.parseInt(spl[1]);
+        return 60 * hours + minutes;
     }
 
     public static String getWikiLink() {
---
+++ src/de/eliaspr/Lecture.java
@@ -0,0 +1,67 @@
+package de.eliaspr;
+
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.TextChannel;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class Lecture {
+
+    public final int startTime, endTime;
+    public final String name;
+
+    public Lecture(int startTime, int endTime, String name) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.name = name;
+    }
+
+    public static Lecture getCurrentLecture(Guild guild) {
+        Calendar c = Calendar.getInstance();
+        int dayAgeMinutes = c.get(Calendar.HOUR_OF_DAY) * 60 + c.get(Calendar.MINUTE);
+        for (Lecture lecture : getAllToday(guild)) {
+            if (lecture.startTime < dayAgeMinutes && lecture.endTime > dayAgeMinutes) {
+                return lecture;
+            }
+        }
+        return null;
+    }
+
+    public static ArrayList<Lecture> getAllToday(Guild guild) {
+        Calendar c = Calendar.getInstance();
+        ArrayList<Lecture> list = new ArrayList<>();
+        for (TextChannel channel : guild.getTextChannelsByName("ankündigungen", true)) {
+            for (Message message : channel.getHistoryAround(channel.getLatestMessageId(), 10).complete().getRetrievedHistory()) {
+                String textMsg = message.getContentRaw();
+                if (textMsg.contains("Stundenplan für morgen")) {
+                    String date = textMsg.substring(0, 8);
+                    if (date.equals(String.format("%02d.%02d.%02d", c.get(Calendar.DAY_OF_MONTH) + 1, c.get(Calendar.MONTH) + 1, c.get(Calendar.YEAR) % 100))) {
+                        String[] msgLines = textMsg.split("\n");
+                        for (String line : msgLines) {
+                            line = line.trim();
+                            if (line.startsWith("- ")) {
+                                line = line.substring(2);
+                                int uhrInx;
+                                String time = line.substring(0, uhrInx = line.indexOf(" Uhr"));
+                                String lectureName = line.substring(line.indexOf(':', uhrInx) + 1);
+                                String[] timeStamps = time.split("-");
+                                list.add(new Lecture(parseTimeStamp(timeStamps[0]), parseTimeStamp(timeStamps[1]), lectureName));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return list;
+    }
+
+    private static int parseTimeStamp(String timeStamp) {
+        String[] spl = timeStamp.split(":");
+        int hours = Integer.parseInt(spl[0]);
+        int minutes = Integer.parseInt(spl[1]);
+        return 60 * hours + minutes;
+    }
+
+}
